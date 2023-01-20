b803aebbf41c2f5fac8f58215d86f10a80c06088
Jaffar Abbas <gamaportal8@gmail.com>
2023-01-20 21:48:03 +0500

    1-20-2023

---
+++ flutter_tip_calculater/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tip_calculater/View/home_view.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
